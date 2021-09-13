0b8242d23aee74f4432eb3b878dc771e53ae981b
unknown <archerzou>
2021-09-13 21:44:04 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
+
+const users = new Collection('http://localhost:3000/users',
+    (json: UserProps) => {
+        return User.bulidUser(json);
+    }
+);
+
+users.on('change', ()=> {
+    const root = document.getElementById('root');
+
+    if (root) {
+        new UserList(root, users).render();
+    }
+});
+
+users.fetch();
+
+// import { UserEdit } from "./views/UserEdit";
+// import { User } from "./models/User";
+
+// const user = User.bulidUser({ name: 'NAME', age: 20 });
+
+// const root = document.getElementById('root');
+
+// if (root) {
+//     const userEdit = new UserEdit( root, user);
+//     userEdit.render();
+//     console.log(userEdit);
+// } else {
+//     throw new Error('Root element not found');
+// }
+
+
+
